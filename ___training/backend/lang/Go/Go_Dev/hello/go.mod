3caa4076c85ae34ba4feabb1cea4c9aea758dfc4
Camilo Sijindioy <31936252+csijindioy@users.noreply.github.com>
2024-09-10 01:00:05 -0500

    Go dev tutorial new files

--- ___training/backend/lang/Go/Go_Dev/hello/go.mod
+++ ___training/backend/lang/Go/Go_Dev/hello/go.mod
@@ -1,10 +1,7 @@
-module hello
+module example.com/hello
 
 go 1.23.1
 
-require rsc.io/quote v1.5.2
+replace example.com/greetings => ../greetings
 
-require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
-	rsc.io/sampler v1.3.0 // indirect
-)
+require example.com/greetings v0.0.0-00010101000000-000000000000
--- ___training/backend/lang/Go/Go_Dev/hello/hello.go
+++ ___training/backend/lang/Go/Go_Dev/hello/hello.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-import "rsc.io/quote"
+	"example.com/greetings"
+)
 
 func main() {
-	fmt.Println(quote.Go())
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, spurce file and line number.
+	log.SetPrefix("Greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
 }
