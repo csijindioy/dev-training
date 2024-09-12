9b5d9303a918b66f00400371eac996c0e28b4ad2
Camilo Sijindioy <31936252+csijindioy@users.noreply.github.com>
2024-09-11 21:46:36 -0500

    Return a random greeting tutorial

--- ___training/backend/lang/Go/Go_Dev/greetings/greetings.go
+++ ___training/backend/lang/Go/Go_Dev/greetings/greetings.go
@@ -3,7 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
-
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
@@ -15,6 +15,21 @@ func Hello(name string) (string, error) {
 
 	// if a name was received, return a value that embeds the name
 	// in a greeting message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// randomFormat reurns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifing
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
