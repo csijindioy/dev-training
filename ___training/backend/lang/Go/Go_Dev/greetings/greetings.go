73ef4e3e048d38b4ba64e761ebebc2618a8aa4ca
Camilo Sijindioy <31936252+csijindioy@users.noreply.github.com>
2024-09-11 22:31:41 -0500

    Return greetings for multiple people

--- ___training/backend/lang/Go/Go_Dev/greetings/greetings.go
+++ ___training/backend/lang/Go/Go_Dev/greetings/greetings.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+
 )
 
 // Hello returns a greeting for the named person.
@@ -13,10 +14,34 @@ func Hello(name string) (string, error) {
 		return "", errors.New("Empty name")
 	}
 
+	// Create a message using a random format.
 	// if a name was received, return a value that embeds the name
 	// in a greeting message.
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages
+	messages := make(map[string]string)
+
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+
+		// In the map, associate the retrieved message
+		// with the name.
+		messages[name] = message
+	}
+
+	return messages, nil
 }
 
 // randomFormat reurns one of a set of greeting messages. The returned
