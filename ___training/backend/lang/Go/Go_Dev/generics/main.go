d01ffb30d0851e382906d9f362468c68256ad55a
Camilo Sijindioy <31936252+csijindioy@users.noreply.github.com>
2024-09-22 22:48:33 -0500

    Getting started with generics

--- ___training/backend/lang/Go/Go_Dev/generics/main.go
+++ ___training/backend/lang/Go/Go_Dev/generics/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Number interface {
+	int64 | float64
+}
 
 func main() {
 	// Initialize a map for the integer values
@@ -15,9 +21,23 @@ func main() {
 		"second": 26.99,
 	}
 
-	fmt.Printf("Non-Generic Sums: %v and %v\n",
+	fmt.Printf(
+		"Non-Generic Sums: %v and %v\n",
 		SumInts(ints),
-		SumFloats(floats))
+		SumFloats(floats),
+	)
+
+	fmt.Printf(
+		"Generic Sums: %v and  %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats),
+	)
+
+	fmt.Printf(
+		"Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats),
+	)
 }
 
 // SumInts adds together the value of m.
@@ -34,6 +54,30 @@ func SumInts(m map[string]int64) int64 {
 // SumFloats adds together the value of m.
 func SumFloats(m map[string]float64) float64 {
 	var s float64
+
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
+
+// SumIntsOrFloats sums the value of map m. It support both int64 and float64
+// as types for map values.
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
+
+// SumNumbers sums the value of map m. It supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
 
 	for _, v := range m {
 		s += v
